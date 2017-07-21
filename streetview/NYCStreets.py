3a08ddef45a522e8605b22a0c65f0b497f7d311a
narges razavian <narges.razavian@nyumc.org>
2017-07-20 20:58:51 -0400

    update

--- streetview/NYCStreets.py
+++ streetview/NYCStreets.py
@@ -4,7 +4,7 @@ import urllib.request
 import urllib.parse
 
 
-def query_api(keyfile='streetview_key.secret', addresslist=['227 east 30st, NY', '95 lexington ave, NYC 10016']):
+def query_api(keyfile='streetview_key.secret', addresslist=['227 east 30st, NY', '220 east 30th street, NYC 10016']):
     try:
         keystr = open(keyfile).readline().strip()
     except:
@@ -49,7 +49,7 @@ def street_view_save(address='227 east 30st, NY 10016', gmaps = None, keystr1 = 
     urllib.request.urlretrieve(MyUrl, fname)
     print(fname, MyUrl)
 
-def crowl_n_save(toplat=40.9229, toplon=-73.9949, botlat=40.5322, botlon=-73.5453, step=0.0005, keyfile1='streetview_key.secret'):
+def crowl_and_save(toplat=40.9229, toplon=-73.9949, botlat=40.5322, botlon=-73.5453, step=0.0005, keyfile1='streetview_key.secret'):
     try:
         keystr1 = open(keyfile1).readline().strip()
     except:
